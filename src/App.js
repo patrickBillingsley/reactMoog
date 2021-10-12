88804b25f74df7e1d600a4c835a75a3281257053
Patrick Billingsley <prbillingsley89@gmail.com>
2021-10-12 09:18:14 -0500

    Convert App.js to a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Synth from './components/SynthComponent';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Synth />
-      </div>
-    );
-  };
+const App = () => {
+    return <Synth />
 };
 
 export default App;

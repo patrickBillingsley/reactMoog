404b9887fbef4a3653a0153e1b2c48c9cf01eda2
Patrick Billingsley <prbillingsley89@gmail.com>
2021-10-14 12:02:59 -0500

    Make Keyboard Control Master Gain Node

---
+++ src/components/KeyboardFunctionalComponent.js
@@ -0,0 +1,54 @@
+import React, { useContext } from 'react';
+import { ACTIONS, SynthCtx } from './SynthFunctionalComponent';
+
+const notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
+
+function generateOctaves(n) {
+    if(n === 1) {
+        return notes;
+    }
+    return notes.concat(generateOctaves(n-1));
+}
+
+const Keys = () => {
+    const ctx = useContext(SynthCtx);
+    let octave = -1;
+
+    return(
+        generateOctaves(3).map((note, index) => {
+            if(index % 12 === 0) octave++;
+
+            const isFlat = note[1] ? false : true;
+            const keyColor = isFlat ? 'black' : 'white';
+
+            return(
+                <div
+                    key={note + octave}
+                    className={`key key__${keyColor} border border-top-0 border-secondary`}
+                    data-note={note}
+                    data-octave={octave}
+                    onMouseEnter={({target}) => {
+                        ctx.dispatch({
+                            type: ACTIONS.CHANGE_NOTE,
+                            payload: {
+                                note: target.dataset.note,
+                                octave: +target.dataset.octave
+                            }
+                        })
+                    }}
+                    onMouseLeave={() => ctx.dispatch({ type: ACTIONS.STOP })}
+                />
+            );
+        })
+    );
+}
+
+const Keyboard = () => {
+    return(
+        <div className='row keys border-top'>
+            <Keys />
+        </div>
+    );
+};
+
+export default Keyboard;
--- src/components/SynthFunctionalComponent.js
+++ src/components/SynthFunctionalComponent.js
@@ -1,22 +1,30 @@
 import React, { useReducer } from 'react';
-import { createOscillator } from './OscillatorFunctionalComponent';
+import { createOscillator } from '../functions/createOscillator';
+import Keyboard from './KeyboardFunctionalComponent';
 import Controllers from './ControllersFunctionalComponent';
+import { FREQUENCIES } from '../shared/frequencies';
 
 const ctx = new (window.AudioContext || window.webkitAudioContext)();
 const masterGain = ctx.createGain();
 let [osc1, gain1] = createOscillator(ctx, masterGain);
 
-masterGain.gain.setValueAtTime(0.5, ctx.currentTime);
+masterGain.gain.setValueAtTime(0, ctx.currentTime);
 masterGain.connect(ctx.destination);
 
 export const SynthCtx = React.createContext();
 
 const initialState = {
-    tune: 0
+    tune: 0,
+    note: 'A',
+    octave: 4,
+    masterVol: 0.5,
+    isPlaying: false
 };
 
 export const ACTIONS = {
-    TUNE: 'tune'
+    TUNE: 'tune',
+    CHANGE_NOTE: 'change note',
+    STOP: 'stop'
 };
 
 function reducer(state, action) {
@@ -24,6 +32,14 @@ function reducer(state, action) {
         case ACTIONS.TUNE:
             osc1.detune.setValueAtTime(action.payload.value, ctx.currentTime);
             return { ...state, tune: action.payload.value};
+        case ACTIONS.CHANGE_NOTE:
+            let {octave, note} = action.payload;
+            masterGain.gain.linearRampToValueAtTime(state.masterVol, ctx.currentTime);
+            osc1.frequency.linearRampToValueAtTime(FREQUENCIES[octave + 2][note], ctx.currentTime);
+            return { ...state, note: action.payload.note, octave: action.payload.octave }
+        case ACTIONS.STOP:
+            masterGain.gain.linearRampToValueAtTime(0, ctx.currentTime);
+            return { ...state, isPlaying: false }
         default:
             return state;
     }
@@ -37,6 +53,7 @@ const Synth = () => {
             <div className='row control-face align-items-stretch'>
                 <SynthCtx.Provider value={{ state: state, dispatch: dispatch, ctx: ctx }}>
                     <Controllers />
+                    <Keyboard />
                 </SynthCtx.Provider>
             </div>
         </div>

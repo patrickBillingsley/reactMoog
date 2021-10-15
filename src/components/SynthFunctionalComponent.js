86b8ce52535d973a8109cf5491d03f24ad5e0473
Patrick Billingsley <prbillingsley89@gmail.com>
2021-10-15 12:54:53 -0500

    Create Keybindings

--- src/components/SynthFunctionalComponent.js
+++ src/components/SynthFunctionalComponent.js
@@ -1,29 +1,38 @@
 import React, { useReducer } from 'react';
 import { reducer } from '../functions/reducer';
+import { initialState } from '../presets/initialState';
 import { createOscillatorBank } from '../functions/createOscillatorBank';
+import { KEYBINDINGS } from '../shared/KEYBINDINGS';
 import Controllers from './ControllersFunctionalComponent';
 import Keyboard from './KeyboardFunctionalComponent';
 
+export const SynthCtx = React.createContext();
 export const ctx = new (window.AudioContext || window.webkitAudioContext)();
 export const masterGain = ctx.createGain();
 export const oscillatorBank = createOscillatorBank(3, ctx);
+const heldKeys = [];
 
-oscillatorBank[0].gain.connect(masterGain);
-oscillatorBank[1].gain.connect(masterGain);
-oscillatorBank[2].gain.connect(masterGain);
+init();
 
-masterGain.gain.setValueAtTime(0, ctx.currentTime);
-masterGain.connect(ctx.destination);
+const keybindings = KEYBINDINGS;
 
-export const SynthCtx = React.createContext();
+window.addEventListener('keydown', e => {
+    const index = keybindings.indexOf(e.code);
+    if(!keybindings.includes(e.code) || heldKeys.includes(index)) return;
+    if(index > -1 && !heldKeys.includes(index)) {
+        heldKeys.push(index);
+        heldKeys.sort((a, b) => sortHighToLow(a, b));
+    }
+    console.log(heldKeys);
+})
 
-const initialState = {
-    tune: 0,
-    note: 'A',
-    octave: 4,
-    masterVol: 0.5,
-    isPlaying: false
-};
+window.addEventListener('keyup', e => {
+    const index = keybindings.indexOf(e.code);
+    if(heldKeys.includes(index)) {
+        heldKeys.splice(heldKeys.indexOf(index), 1);
+    }
+    console.log(heldKeys);
+})
 
 const Synth = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
@@ -39,5 +48,17 @@ const Synth = () => {
         </div>
     );
 };
+
+function init() {
+    oscillatorBank.forEach(osc => osc.gain.connect(masterGain));
+    masterGain.gain.setValueAtTime(0, ctx.currentTime);
+    masterGain.connect(ctx.destination);
+}
+
+function sortHighToLow(a, b) {
+    return a > b ? -1 :
+           a < b ?  1 :
+                    0
+}
 
 export default Synth;
--- src/functions/reducer.js
+++ src/functions/reducer.js
@@ -1,21 +1,21 @@
-import { oscillatorBank, masterGain, ctx } from '../components/SynthFunctionalComponent';
+import * as context from '../components/SynthFunctionalComponent';
 import { ACTIONS } from '../shared/actions';
 import { FREQUENCIES } from '../shared/frequencies';
 
 export function reducer(state, action) {
     switch(action.type) {
         case ACTIONS.TUNE:
-            oscillatorBank[0].osc.detune.setValueAtTime(action.payload.value, ctx.currentTime);
+            context.oscillatorBank[0].osc.detune.setValueAtTime(action.payload.value, context.ctx.currentTime);
             return { ...state, tune: action.payload.value};
         case ACTIONS.CHANGE_NOTE:
             let {octave, note} = action.payload;
-            masterGain.gain.linearRampToValueAtTime(state.masterVol, ctx.currentTime);
-            oscillatorBank.forEach(osc => {
-                osc.osc.frequency.linearRampToValueAtTime(FREQUENCIES[octave + 3][note], ctx.currentTime);
+            context.masterGain.gain.linearRampToValueAtTime(state.masterVol, context.ctx.currentTime);
+            context.oscillatorBank.forEach(osc => {
+                osc.osc.frequency.linearRampToValueAtTime(FREQUENCIES[octave + 3][note], context.ctx.currentTime);
             })
             return { ...state, note: action.payload.note, octave: action.payload.octave }
         case ACTIONS.STOP:
-            masterGain.gain.linearRampToValueAtTime(0, ctx.currentTime);
+            context.masterGain.gain.linearRampToValueAtTime(0, context.ctx.currentTime);
             return { ...state, isPlaying: false }
         default:
             return state;
